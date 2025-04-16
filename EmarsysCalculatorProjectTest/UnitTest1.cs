fc8db1e952fe90ad7cfa3c81c1328835c91807eb
Tyland Sheard <77373702+Tyland01@users.noreply.github.com>
2025-04-16 11:20:16 -0400

    UnitTest1.cs

--- EmarsysCalculatorProjectTest/UnitTest1.cs
+++ EmarsysCalculatorProjectTest/UnitTest1.cs
@@ -1,10 +1,25 @@
-namespace EmarsysCalculatorProjectTest;
+using System;
+using Xunit;
+using EmarsysCalculatorProject1;
 
-public class UnitTest1
+namespace EmarsysCalculatorProjectTest
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
+        [Fact]
+        public void CalculateDueDate_ShouldReturnMondayAt4PM_WhenSubmittedFridayAt4PMWith16HourTurnaround()
+        {
+            // Arrange
+            var calculator = new DueDateCalculator();
+            var submitTime = new DateTime(2024, 4, 12, 16, 0, 0); // Friday, 4:00 PM
+            var turnaroundHours = 16;
 
+            // Act
+            var dueDate = calculator.CalculateDueDate(submitTime, turnaroundHours);
+
+            // Assert
+            var expectedDueDate = new DateTime(2024, 4, 15, 16, 0, 0); // Monday, 4:00 PM
+            Assert.Equal(expectedDueDate, dueDate);
+        }
     }
 }
